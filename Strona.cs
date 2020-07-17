212beca2ea256f4c7c78cb4767b33df85ec3fa99
HITMANRAFI <68203812+HITMANRAFI@users.noreply.github.com>
2020-07-17 15:08:20 +0200

    Add files via upload

---
+++ Strona.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Strona : MonoBehaviour
+{
+    public void Rejestracja()
+    {
+        Application.OpenURL("https://www.youtube.com/watch?v=oHg5SJYRHA0");
+    }
+}
