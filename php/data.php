479eb888d1fa93e70b841cbf657fff07e1e23ebd
Typowy <typowystahu@gmail.com>
2020-07-18 04:09:35 +0200

    poprawka systemu logowania

--- php/data.php
+++ php/data.php
@@ -17,13 +17,11 @@ $conn = new mysqli($servername, $db_username, $db_password, $db_name);
 if ($conn->connect_error) {
 	die("Błąd połączenia: " . $conn->connect_error);
 }
-echo "Połączono z bazą. ";
-
 // Login -- protected
 //https://www.hacksplaining.com/exercises/sql-injection#/start
 //https://www.w3schools.com/sql/sql_injection.asp
 
-$sql = "SELECT Password FROM users WHERE Login = ?"; //https://www.w3schools.com/php/php_mysql_select.asp
+$sql = "SELECT UserID, UserPassword FROM users_login WHERE UserEmail = ?"; //https://www.w3schools.com/php/php_mysql_select.asp
 
 $statement = $conn -> prepare($sql);
 $statement -> bind_param("s", $loginUser);
@@ -34,8 +32,8 @@ $result = $statement -> get_result();
 if ($result->num_rows > 0) {
 	// output data of each row
 	while($row = $result->fetch_assoc()) {
-		if($row["Password"] == $loginPass){
-			echo "Zalogowano pomyślnie! ";
+		if($row["UserPassword"] == $loginPass){
+			echo $row['UserID'];
 		} else {
 			echo "Niepoprawny Login lub Hasło. ";
 		}
