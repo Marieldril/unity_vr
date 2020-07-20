c5ea971f6475c6393240d1d855fa956634848de8
Typowy <typowystahu@gmail.com>
2020-07-20 15:39:35 +0200

    c# SQLSendScore update

--- scripts/SQLSendScore.cs
+++ scripts/SQLSendScore.cs
@@ -8,20 +8,23 @@ using UnityEngine.SceneManagement;
 public class SQLSendScore : MonoBehaviour
 {
     public Button button_send;
-    
-    
+    public int IntPoints;
+
     void Start()
     {
         // Convert index of scene to string
         int IntScene = SceneManager.GetActiveScene().buildIndex;
         string StrScene = IntScene.ToString();
 
+        // Convert sum of points
+        string StrPoints = IntPoints.ToString();
+
         // A correct website page.
         StartCoroutine(WebPHPConnect.GetRequest("http://localhost/unity_vr_sql/sendScore.php"));
 
         // Send UserID, Question number and Points to SQL Function
         button_send.onClick.AddListener(() => {
-            StartCoroutine(WebPHPConnect.Send(WebPHPConnect.CurrentID, StrScene, "1"));
+            StartCoroutine(WebPHPConnect.Send(WebPHPConnect.CurrentID, StrScene, StrPoints));
         });
     }
 }
